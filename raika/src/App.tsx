13bb1ccf71a8d6128718f417ba9e6d4c7db52b26
AliNikseresht <alinksht@gmail.com>
2024-02-27 14:16:37 +0330

    Implement a secure React component to protect against CSRF attacks with token management and server-side authentication.

---
+++ raika/src/App.tsx
@@ -0,0 +1,15 @@
+import CSRFTokenDemo from './components/CSRFTokenDemo';
+import "./App.css"
+
+function App() {
+  return (
+    <div>
+      <header>
+        <h3>Example implementation of CSRF protection</h3>
+      </header>
+      <CSRFTokenDemo />
+    </div>
+  );
+}
+
+export default App;
